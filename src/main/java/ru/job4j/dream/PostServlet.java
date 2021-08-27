0918a1acf4d80f57017bbc20528bb42cd6de90c4
ListerioAxxe <langua095@gmail.com>
2021-08-27 12:59:24 +0300

    new Post

---
+++ src/main/java/ru/job4j/dream/PostServlet.java
@@ -0,0 +1,20 @@
+package ru.job4j.dream;
+
+import ru.job4j.dream.model.Post;
+import ru.job4j.dream.store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name")));
+         res.sendRedirect(req.getContextPath() + "/posts.jsp");
+    }
+}
