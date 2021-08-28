4487bd5965e63135e9d6b5515fe3a23dfc6b4da0
ListerioAxxe <langua095@gmail.com>
2021-08-28 15:00:57 +0300

    correct

---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,17 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.sql.SQLException;
+
+public class PsqlMain {
+    public static void main(String[] args) throws SQLException {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0, "Java Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        Post post = store.findById(0);
+        System.out.println(post);
+    }
+}
--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,62 +1,16 @@
 package ru.job4j.dream.store;
-
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
-import java.time.LocalDateTime;
+import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class Store {
+public interface Store {
+    Collection<Post> findAllPosts();
 
-    private static final Store INST = new Store();
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
-    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
-    private static AtomicInteger POST_ID = new AtomicInteger(4);
-    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+    Collection<Candidate> findAllCandidates();
 
-    private Store() {
-        posts.put(1, new Post(1, "Junior Java Job", "first chapter", LocalDateTime.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "second chapter", LocalDateTime.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "third chapter", LocalDateTime.now()));
-        candidates.put(1, new Candidate(1, "Junior Java"));
-        candidates.put(2, new Candidate(2, "Middle Java"));
-        candidates.put(3, new Candidate(3, "Senior Java"));
-    }
+    void save(Post post) throws SQLException;
 
-    public static Store instOf() {
-        return INST;
-    }
-
-    public Collection<Post> findAllPosts() {
-        return posts.values();
-    }
-
-    public Collection<Candidate> findAllCandidates() {
-        return candidates.values();
-    }
-
-    public void save(Post post) {
-        if (post.getId() == 0) {
-            post.setId(POST_ID.incrementAndGet());
-        }
-        posts.put(post.getId(), post);
-    }
-
-    public void save(Candidate candidate) {
-        if (candidate.getId() == 0) {
-            candidate.setId(CANDIDATE_ID.incrementAndGet());
-        }
-        candidates.put(candidate.getId(), candidate);
-    }
-
-    public Post findById(int id) {
-        return posts.get(id);
-    }
-
-    public Candidate findByIdCandidate(int id) {
-        return candidates.get(id);
-    }
+    Post findById(int id) throws SQLException;
 }
